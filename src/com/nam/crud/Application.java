1eec6713f6832610eb948041e44c978e7bfbeb40
hp <namstudionsk@gmail.com>
2018-10-12 19:28:21 +0700

    Fix Application

--- src/com/nam/crud/Application.java
+++ src/com/nam/crud/Application.java
@@ -5,16 +5,10 @@ import java.util.Iterator;
 
 public class Application {
 
-    private static Application instance = new Application();
+    private StudentDao studentDao = new StudentDaoImpl();
 
-    private ArrayList<Student>  studentList;
-
-    private Application(){
+    public Application(){
         System.out.println("Application::Constructor");
-    }
-
-    public static Application getInstance(){
-        return instance;
     }
 
     public void run(){
@@ -32,35 +26,17 @@ public class Application {
     private void create(){
         System.out.println("Application::create()");
 
-        studentList = new ArrayList<Student>();
+        studentDao.createStudent(new Student(0,"John",'M',1,"P-11"));
+        studentDao.createStudent(new Student(1,"John", 'M',1,"P-11"));
+        studentDao.createStudent(new Student(2,"Emma", 'F',2,"P-22"));
 
-        studentList.add(createStudent("John", "Doe", 'M',1,"P-11"));
-        studentList.add(createStudent("John", "Smith", 'M',1,"P-11"));
-        studentList.add(createStudent("Emma", "Williams", 'F',2,"P-22"));
-
-    }
-
-    private Student createStudent(String name, String lastname, char sex, int course, String group){
-        Student student = new Student();
-
-        student.setName(name);
-        student.setLastname(lastname);
-        student.setSex(sex);
-        student.setCourse(course);
-        student.setGroup(group);
-
-        return student;
     }
 
     private void read(){
         System.out.println("Application::read()");
 
-        if (studentList == null){
-            return;
-        }
-        for (Student student: studentList) {
+        for (Student student: studentDao.getAllStudents()) {
             System.out.print("Name: " + student.getName());
-            System.out.print(" Lastname: " + student.getLastname());
             System.out.print(" Sex: " + student.getSex());
             System.out.print(" Course: " + student.getCourse());
             System.out.println(" Group: " + student.getGroup());
@@ -70,7 +46,7 @@ public class Application {
     private void update(){
         System.out.println("Application::update()");
 
-        for (Student student: studentList){
+        for (Student student: studentDao.getAllStudents()){
             if (student.getCourse() == 1){
                 student.setCourse(student.getCourse() + 1);
             }
@@ -80,7 +56,7 @@ public class Application {
     private void delete(){
         System.out.println("Application::delete()");
 
-        Iterator iterator = studentList.iterator();
+        Iterator iterator = studentDao.getAllStudents().iterator();
 
         while (iterator.hasNext()){
 
